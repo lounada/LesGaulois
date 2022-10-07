937a9be36c36f0cce07638d194b9a67c39145d8f
lounada <114723938+lounada@users.noreply.github.com>
2022-10-07 09:38:23 +0200

    TP1

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,32 +1,49 @@
 package personnages;
 
-public class Romain {
+import random java.math;
+
+public class Druide {
 	private String nom;
-	private int force;
-	
-	public Romain(String nom, int force) {
+	private int effetPotionMin;
+	private int effetPotionMax;
+	private int forcePotion = 1;
+    public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
-		this.force = force;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
+				+ effetPotionMax + ".");
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
 	
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 
 	private String prendreParole() {
-		return "Le romain " + nom + " : ";
+		return "Le druide " + nom + " : ";
 	}
-	
-	public void recevoirCoup(int forceCoup) {
-		force -= forceCoup;
-		if (force > 0) {
-			parler("Aïe");
+	public void preparerPotion() {
+		Random random = new Random();
+		forcePotion = (random.nextInt(effetPotionMax - effetPotionMin)) + effetPotionMin;
+		String texte = "";
+		if (forcePotion > 7) {
+			texte += "J'ai préparé une super potion ";
 		} else {
-			parler("j'abondonne..");
+			texte += "Je n'ai pas trouvé tous les ingrédients ma potion est seulement de force ";
+		}
+		parler(texte +forcePotion + " .");
+	}
+
+	public void booster(Gaulois gaulois) {
+		if (gaulois.getNom().equals("Obélix")) {
+			parler("Non, Obélix !... Tu n'auras pas de potion magique !");
+		} else {
+			gaulois.boirePotion(forcePotion);
 		}
 	}
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,6 +3,8 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private int nbVillageois=0;
+	private villageois[] gaulois; 
 	
 	public Village(String nom) {
 		this.nom = nom;
@@ -10,7 +12,22 @@ public class Village {
 	public void setChef(Chef chef) {
 		this.chef = chef;
     }
+	
+	
+	}
 	public String getNom() {
 		return nom;
 	}
+	public Village(String nom, Chef chef, int nbVillageois, villageois[] gaulois, int nbVillageoisMaximum) {
+		super();
+		this.nom = nom;
+		this.chef = chef;
+		this.nbVillageois = nbVillageois;
+		this.gaulois = gaulois;
+		this.nbVillageoisMaximum=
+	}
+	public void ajouterHabitant(String Gaulois) {
+		
+	}
+	
 }
