96d2d7886d85f72b1633d098988735f6b949333b
lounada <114723938+lounada@users.noreply.github.com>
2022-10-07 08:32:50 +0200

    TP1

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,15 +1,19 @@
 package personnages;
 
 public class Romain {
-
-	public void recevoirCoup(int i) {
-		// TODO Auto-generated method stub
-		
+	private String nom;
+	private int force;
+	
+	public Romain (String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+		}
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe !");
+        } else {
+        	parler("J'abandonne...");
+        }
 	}
-
-	public String getNom() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
